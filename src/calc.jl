4e55de2b7ce858e65ca4dff1516a87bdfd6129c5
zasyp45 <zasypkin4514@gmail.com>
2026-02-15 15:22:20 +0300

    basic calcs

---
+++ src/calc.jl
@@ -0,0 +1,12 @@
+P = 3e5
+h = 6.877e7
+ρ0 = 1.25
+U_source = 300
+A_source = 0.9
+U = A_source * U_source
+I = P / U
+η1 = 0.75
+P_plasm = P * η1
+G = P_plasm/h
+
+d = (49022^2) * G ^ (1.5)/((270^2)*I) 
